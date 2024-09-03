5519ce156958793b8fb2fde9b5d3714450e2d0b9
Antony Marcos Barja Ingaruca <antony.barja8@gmail.com>
2024-09-03 12:34:06 -0500

    updated

---
+++ tests/testthat/test-get_districts.R
@@ -0,0 +1,14 @@
+test_that("get_districts return a sf object", {
+  resultado <- get_districts()
+  # Verifica que el resultado sea un objeto de clase 'sf'
+  expect_s3_class(resultado, "sf")
+
+  # Verifica que el objeto tiene geometría tipo 'POLYGON' o 'MULTIPOLYGON'
+  expect_true(all(sf::st_geometry_type(resultado) %in% c("POLYGON", "MULTIPOLYGON")))
+
+  # Verifica que no hay geometrías vacías
+  expect_true(all(!sf::st_is_empty(resultado)))
+
+  # Verifica que el número de filas sea mayor que cero
+  expect_gt(nrow(resultado), 0)
+})
