8c89abd3d0b6a9282cf02e18cc76f8e586c318f2
Antony Marcos Barja Ingaruca <antony.barja8@gmail.com>
2024-09-13 21:04:11 -0500

    updated function

--- R/zzz.R
+++ R/zzz.R
@@ -6,14 +6,13 @@
     "geoidep is a wrapper that allows you to download cartographic data for Peru from R.
     Currently, `geoidep` supports the following providers:"
   )
-
   # Lista de proveedores con mensajes de éxito
-  providers <- c("INEI", "MINAM", "MIDAGRI", "SERNANP")
+  providers <- get_providers()[["provider"]] |> as.vector()
   for (provider in providers) {
     cli::cli_alert_success(provider)
   }
 
   cli::cli_alert_info(
-    "For more information, please use the `get_providers()` function."
+    "For more information, please use the `get_data_sources()` function."
   )
 }
