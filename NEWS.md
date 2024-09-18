b6001c391cfe8dcacb2794c7d51fa0c35feed929
Antony Marcos Barja Ingaruca <antony.barja8@gmail.com>
2024-09-18 18:14:13 -0500

    updated onload function

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,5 @@
+# geoidep 1.0.0
+
 # geoidep 0.2.0
 
 * Added a `NEWS.md` file to track changes to the package.
--- R/zzz.R
+++ R/zzz.R
@@ -1,18 +1,18 @@
-.onAttach <- function(...) {
-  # Usar `cli` para formatear el mensaje
-  cli::cli_h1("Welcome to geoidep")
+.onAttach <- function(libname, pkgname) {
+  if (interactive()) {
+    cli::cli_h1("Welcome to geoidep")
 
-  cli::cli_alert_info(
-    "geoidep is a wrapper that allows you to download cartographic data for Peru from R.
-    Currently, `geoidep` supports the following providers:"
-  )
-  # Lista de proveedores con mensajes de éxito
-  providers <- get_providers()[["provider"]] |> as.vector()
-  for (provider in providers) {
-    cli::cli_alert_success(provider)
+    cli::cli_alert_info(
+      "geoidep is a wrapper that allows you to download cartographic data for Peru from R.
+       Currently, `geoidep` supports the following providers:"
+    )
+
+    providers <- get_providers()[["provider"]] |> as.vector()
+
+    for (provider in providers) {
+      cli::cli_alert_success(provider)
+    }
+
+    cli::cli_alert_info("For more information, please use the `get_data_sources()` function.")
   }
-
-  cli::cli_alert_info(
-    "For more information, please use the `get_data_sources()` function."
-  )
 }
