b72158b9fd3a93a2f1a2d120e971153dadd01937
Antony Marcos Barja Ingaruca <antony.barja8@gmail.com>
2025-03-04 22:27:45 -0500

    INEI functions fixed

--- R/get_districts.R
+++ R/get_districts.R
@@ -56,17 +56,16 @@ get_districts <- \(dsn = NULL, show_progress = TRUE, quiet = TRUE) {
   extract_dir <- file.path(tempdir(), "geoidep_data")
   dir.create(extract_dir, recursive = TRUE, showWarnings = FALSE)
   archive::archive_extract(archive = dsn, dir = extract_dir)
-
-  if (file.exists(dsn)) {
-    suppressMessages(invisible(file.remove(dsn)))
-  }
-
   gpkg_file <- dplyr::first(list.files(extract_dir, pattern = "\\.gpkg$", full.names = TRUE))
   suppressMessages(invisible(file.rename(from = gpkg_file, to = tolower(gpkg_file))))
 
   # Validate if .gpkg file exists
   if (length(gpkg_file) == 0) {
     stop("No .gpkg file was found after extraction")
+  }
+
+  if (file.exists(dsn)) {
+    suppressMessages(invisible(file.remove(dsn)))
   }
 
   sf_data <- sf::st_read(gpkg_file, quiet = quiet)
