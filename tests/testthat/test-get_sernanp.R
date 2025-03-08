5445e8fee377917a9a9fdb0430ceae93adcf7de7
Antony Marcos Barja Ingaruca <antony.barja8@gmail.com>
2025-03-08 15:14:47 -0500

    updated

--- tests/testthat/test-get_sernanp.R
+++ tests/testthat/test-get_sernanp.R
@@ -1,7 +1,7 @@
 test_that("R/get_sernanp return a sf object", {
   resultado <- geoidep::get_sernanp_data(layer = "zonificacion_anp" , show_progress = FALSE)
   expect_s3_class(resultado, "sf")
-  expect_true(all(sf::st_geometry_type(resultado) %in% c("POLYGON", "MULTIPOLYGON")))
+  expect_true(all(sf::st_geometry_type(resultado) %in% c("POLYGON", "MULTIPOLYGON", "LINE", "LINSTRING", "POINT", "MULTIPOINT")))
   expect_true(all(!sf::st_is_empty(resultado)))
   expect_gt(nrow(resultado), 0)
 })
