408381b73b5d36b300a1ce665282de7d4da009d4
Antony Marcos Barja Ingaruca <antony.barja8@gmail.com>
2025-03-22 23:04:19 -0500

    new external script and edited get_districts

--- R/get_districts.R
+++ R/get_districts.R
@@ -1,10 +1,20 @@
 #' Download INEI district boundaries
 #'
 #' @description
-#' This function allows you to download the latest version of the \bold{geometry} and \bold{ubigeos}
-#' corresponding to the \bold{official political division} of the district boundaries of Peru.
+#' This function allows you to download the latest version of the **geometry** and **ubigeos**
+#' corresponding to the **official political division** of the district boundaries of Peru.
 #' For more information, you can visit the following page \href{https://ide.inei.gob.pe/}{INEI Spatial Data Portal}.
 #'
+#' @details
+#' `r lifecycle::badge("stable")`
+#' This function is considered stable and its API is unlikely to change in future versions.
+#'
+#' If no values are provided for the `departamento`, `provincia`, or `distrito` parameters, the function
+#' will download and return the complete dataset containing district boundaries for the entire country.
+#'
+#' @param departamento Character. Name of the level-1 administrative boundary (department) to query. The input is case-insensitive.
+#' @param provincia Character. Name of the level-2 administrative boundary (province) to query. The input is case-insensitive.
+#' @param distrito Character. Name of the level-3 administrative boundary (district) to query. The input is case-insensitive.
 #' @param dsn Character. Path to the output \code{.gpkg} file or a directory where the file will be saved.
 #' If a directory is provided, the file will be saved as \code{DISTRITO.gpkg} inside it.
 #' If \code{NULL}, a temporary file will be created.
@@ -18,16 +28,16 @@
 #' @examples
 #' \donttest{
 #' library(geoidep)
+#' # Download all district boundaries (entire country)
 #' dist <- get_districts(show_progress = FALSE)
 #' head(dist)
 #' }
-#' @export
-
-get_districts <- \(dsn = NULL, show_progress = TRUE, quiet = TRUE, timeout = 60) {
+get_districts <- \(departamento = NULL, provincia = NULL, distrito = NULL,
+                   dsn = NULL, timeout = 60, show_progress = TRUE, quiet = TRUE) {
   primary_link <- get_inei_link("distrito")
 
   if (is.null(dsn)) {
-    dsn <-  tempfile(pattern = "distrito", fileext = ".rar")
+    dsn <- tempfile(pattern = "distrito", fileext = ".rar")
   } else {
     if (!dir.exists(dsn)) {
       dir.create(dsn, recursive = TRUE, showWarnings = FALSE)
@@ -41,7 +51,8 @@ get_districts <- \(dsn = NULL, show_progress = TRUE, quiet = TRUE, timeout = 60)
         primary_link,
         config = c(
           httr::config(ssl_verifypeer = FALSE),
-          httr::timeout(seconds = timeout)),
+          httr::timeout(seconds = timeout)
+        ),
         httr::write_disk(dsn, overwrite = TRUE),
         httr::progress()
       )
@@ -50,20 +61,20 @@ get_districts <- \(dsn = NULL, show_progress = TRUE, quiet = TRUE, timeout = 60)
         primary_link,
         config = c(
           httr::config(ssl_verifypeer = FALSE),
-          httr::timeout(seconds = timeout)),
+          httr::timeout(seconds = timeout)
+        ),
         httr::write_disk(dsn, overwrite = TRUE)
       )
     }
-  },error = function(e){
-    stop("Error during download:", conditionMessage(e))
-  }
-  )
+  }, error = function(e) {
+    stop("Error during download: ", conditionMessage(e))
+  })
 
   if (httr::http_error(rar.download)) {
     stop("Error downloading the file. Check the URL or connection")
   }
 
-  extract_dir <- file.path(tempdir(), paste0("geoidep_data_","distrito"))
+  extract_dir <- file.path(tempdir(), paste0("geoidep_data_", "distrito"))
   dir.create(extract_dir, recursive = TRUE, showWarnings = FALSE)
   archive::archive_extract(archive = dsn, dir = extract_dir)
   gpkg_files <- dplyr::first(list.files(extract_dir, pattern = "\\.gpkg$", full.names = TRUE))
@@ -89,8 +100,21 @@ get_districts <- \(dsn = NULL, show_progress = TRUE, quiet = TRUE, timeout = 60)
   }
 
   sf_data <- sf::st_read(new_gpkg_file, quiet = quiet)
+
+  # Convert column names to lower case (except geometry)
   non_geom_idx <- which(!grepl("^(geom|geometry)$", names(sf_data), ignore.case = TRUE))
   names(sf_data)[non_geom_idx] <- tolower(names(sf_data)[non_geom_idx])
+
+  # New filter Applied by R users
+  if (!is.null(departamento)) {
+    sf_data <- sf_data[tolower(sf_data$nombdep) == tolower(departamento), ]
+  }
+  if (!is.null(provincia)) {
+    sf_data <- sf_data[tolower(sf_data$nombprov) == tolower(provincia), ]
+  }
+  if (!is.null(distrito)) {
+    sf_data <- sf_data[tolower(sf_data$nombdist) == tolower(distrito), ]
+  }
 
   return(sf_data)
 }
--- R/utils.R
+++ R/utils.R
@@ -1,57 +1,31 @@
 #' Reading a csv containing geoidep resources
 #' @importFrom utils read.csv
 #' @keywords internal
-get_data <- \(){
-  read.csv("https://raw.githubusercontent.com/ambarja/geoidep/main/inst/sources-idep/sources_geoidep.csv")
+get_data <- \(url = NULL){
+  if(is.null(url)){
+    url <- getOption(x = "geoidep", default = .internal_urls$geoidep)
+  }
+  tryCatch({
+    data <- read.csv(url) |> tidyr::as_tibble()
+    return(data)
+  }, error = function(e) {
+    stop("The file could not be read. Check that the link is valid and your Internet connection.")
+  })
 }
+
 
 #' Global variables for get_early_warning
 #' This code declares global variables used in the `get_early_warning` function to avoid R CMD check warnings.
 #' @name global-variables
 #' @keywords internal
-utils::globalVariables(c("X", "Y", "coords", "all_coords", "everything", "lng", "lat","provider","available_providers","loreto_prov",".","FECREG","FECHA","created_date","last_edited_date","emision","extract_meteorological_table","data"))
+utils::globalVariables(c(".internal_urls","X", "Y", "coords", "all_coords", "everything", "lng", "lat","provider","available_providers","loreto_prov",".","FECREG","FECHA","created_date","last_edited_date","emision","extract_meteorological_table","data"))
 
 #' Retrieve links to SERNANP for information on natural protected areas.
 #' @param type A string. Select only one from the list of available layers, for more information please use `get_data_sources(provider = "sernanp")`. Defaults to NULL.
 #' @return A string containing the URL of the requested file.
 #' @keywords internal
 get_sernanp_link <- \(type = NULL){
-  sernanp_layer <-  c(
-    "anp_nacional",
-    "zona_reservada",
-    "acr",
-    "acp",
-    "zona_amortiguamiento",
-    "zonificacion_anp",
-    "zonificacion_acr",
-    "zonificacion_acp",
-    "contratos_aprovechamiento",
-    "acuerdos_actividad_menor",
-    "contrato_servicio_turistico",
-    "autorizacion_predio_privado",
-    "reserva_biosfera_nacional",
-    "reserva_biosfera_transfronterizo",
-    "diagnostico_recursos",
-    "planes_manejo_recursos",
-    "acuiferos",
-    "unidades_hidrograficas",
-    "autoridad_local_agua",
-    "reservas_indigenas",
-    "cc_pueblos_indigenas",
-    "cn_pueblos_indigenas",
-    "reservas_piaci",
-    "clasificacion_climatica",
-    "red_estaciones",
-    "bosque_locales",
-    "ecosistemas_fragiles",
-    "bosque_produccion_permanente",
-    "bosques_protectores",
-    "mapa_ecosistemas_2018",
-    "ecorregiones_cdc")
-  sernanp_service <- "https://geoservicios.sernanp.gob.pe/arcgis/rest/services/sernanp_visor/servicio_descarga/MapServer"
-  sernanp_link <- sprintf("%s/%s/%s",sernanp_service,1:length(sernanp_layer),"query")
-  names(sernanp_link) <- sernanp_layer
-
+  sernanp_link <- getOption("sernanp", default = .internal_urls$sernanp)
   if (!type %in% names(sernanp_link) || is.null(type)) {
     stop("Invalid type. Please choose one layer according sernanp layer. More information use `get_data_sources(providers = 'Sernanp')`")
   }
@@ -65,16 +39,10 @@ get_sernanp_link <- \(type = NULL){
 #' @keywords internal
 
 get_inei_link <- \(type = NULL) {
-  inei_links <- c(
-      distrito = "https://ide.inei.gob.pe/files/Distrito.rar",
-      provincia = "https://ide.inei.gob.pe/files/Provincia.rar",
-    departamento = "https://ide.inei.gob.pe/files/Departamento.rar"
-  )
-
+  inei_links <- getOption("inei", default = .internal_urls$inei)
   if (!type %in% names(inei_links) || is.null(type)) {
     stop("Invalid type. Please choose from 'districto', 'provincia', or 'departmento'.")
   }
-
   return(inei_links[[type]])
 }
 
@@ -83,10 +51,7 @@ get_inei_link <- \(type = NULL) {
 #' @return A string containing the URL of the requested file.
 #' @keywords internal
 get_midagri_link <- \(type = NULL){
-  midagri_link <- c(
-    "agriculture_sector" = "https://siea.midagri.gob.pe/portal/media/attachments/publicaciones/superficie/sectores/2024/SectoresEstadisticos_2024_04.zip",
-    "oil_palm_areas" = "https://siea.midagri.gob.pe/portal/media/attachments/publicaciones/superficie/temas/PALMA_ACEITERA_2016_2020.zip"
-    )
+  midagri_link <- getOption("midagri", default = .internal_urls$midagri)
   if (!type %in% names(midagri_link) || is.null(type)) {
     stop("Invalid type. Please choose from 'agriculture_sector' or 'oil_palm'")
   }
@@ -98,11 +63,7 @@ get_midagri_link <- \(type = NULL){
 #' @return A string containing the URL of the requested file.
 #' @keywords internal
 get_geobosque_link <- \(type = NULL){
-  geobosque_link <- c(
-    "stock_bosque_perdida_distrito" = "http://geobosques.minam.gob.pe/geobosque/ws/rest/BOSQUEPERDIDA/stockBosquePerdidaDistrito",
-    "stock_bosque_perdida_provincia" = "http://geobosques.minam.gob.pe/geobosque/ws/rest/BOSQUEPERDIDA/stockBosquePerdidaProvincia",
-    "stock_bosque_perdida_departamento" = "http://geobosques.minam.gob.pe/geobosque/ws/rest/BOSQUEPERDIDA/stockBosquePerdidaRegion"
-  )
+  geobosque_link <- getOption("geobosque", default = .internal_urls$geobosque)
   if (!type %in% names(geobosque_link) || is.null(type)) {
     stop("Invalid type. Please choose from 'dist', 'prov' or 'dep'")
   }
@@ -115,7 +76,7 @@ get_geobosque_link <- \(type = NULL){
 #' @keywords internal
 
 get_early_warning_link <- \(type = NULL){
-  geobosque_early_warning_link <- c("warning_last_week" = "http://geobosques.minam.gob.pe/geobosque/ws/rest/ALERTAS/ultimasByCobertura")
+  geobosque_early_warning_link <- getOption("geobosque", default = .internal_urls$geobosque)
   if (!type %in% names(geobosque_early_warning_link) || is.null(type)) {
     stop("Invalid type. Please choose 'warning_last_week'")
   }
@@ -127,7 +88,7 @@ get_early_warning_link <- \(type = NULL){
 #' @return A string containing the URL of the requested file.
 #' @keywords internal
 get_heat_spot_link <- \(type = NULL){
-  serfor_heat_spot_link <- c("heat_spot" = "https://geo.serfor.gob.pe/geoservicios/rest/services/Servicios_OGC/Unidad_Monitoreo_Satelital/MapServer/0/query")
+  serfor_heat_spot_link <- getOption("geobosque", default = .internal_urls$geobosque)
   if (!type %in% names(serfor_heat_spot_link) || is.null(type)) {
     stop("Invalid type. Please choose 'heat_spot'")
   }
@@ -149,39 +110,10 @@ as_data_time <- \(x){
 #' @return A string containing the URL of the requested file.
 #' @keywords internal
 get_mtc_link <- \(type = NULL){
-  mtc_layer <- c(
-    "aerodromos_2023" = "https://swmapas.mtc.gob.pe:8443/geoserver/geoportal/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=pe_mtc_018_aerodromos_dic23&%20target=",
-    "aerodromos_2022" = "https://swmapas.mtc.gob.pe:8443/geoserver/geoportal/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=pe_mtc_018_aerodromos_dic22&%20target=",
-    "campamentos_pvn" = "https://swmapas.mtc.gob.pe:8443/geoserver/geoportal/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=gpt_ogpp_campamento_emergencia&maxFeatures=500&%20target=",
-    "centros_acopio" = "https://swmapas.mtc.gob.pe:8443/geoserver/geoportal/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=gpt_ogpp_centro_acopio&maxFeatures=500&%20target=",
-    "concesiones_viales" = "https://swmapas.mtc.gob.pe:8443/geoserver/geoportal/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=geoportal:vw_gli_ogpp_concesion_vial&maxFeatures=500&%20target=",
-    "emergencias_viales" = "https://swmapas.mtc.gob.pe:8443/geoserver/geoportal/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=gpt_ogpp_emergencia_vial&maxFeatures=500&%20target=",
-    "estaciones_pesaje" = "https://swmapas.mtc.gob.pe:8443/geoserver/geoportal/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=pe_mtc_018_pesajes_dic22&%20target=",
-    "estaciones_ferroviarias" = "https://swmapas.mtc.gob.pe:8443/geoserver/geoportal/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=pe_mtc_018_estaciones_ferroviarias&%20target=",
-    "localidades_telecomunicaciones" = "https://swmapas.mtc.gob.pe:8443/geoserver/geoportal/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=gpt_ogpp_poblado_cobertura_telecomunicacion&maxFeatures=500&%20target=",
-    "puentes" = "https://swmapas.mtc.gob.pe:8443/geoserver/geoportal/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=gpt_ogpp_puente&maxFeatures=500&%20target=",
-    "puntos_serpost" = "https://swmapas.mtc.gob.pe:8443/geoserver/geoportal/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=gpt_ogpp_serpost&maxFeatures=500&%20target=",
-    "red_ferroviaria" = "https://swmapas.mtc.gob.pe:8443/geoserver/geoportal/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=pe_mtc_018_red_ferroviaria_dic22&%20target=",
-    "red_vial_departamental_2022" = "https://swmapas.mtc.gob.pe:8443/geoserver/geoportal/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=pe_mtc_018_red_vial_departamental_dic22&maxFeatures=1000&%20target=",
-    "red_vial_departamental_2023" = "https://swmapas.mtc.gob.pe:8443/geoserver/geoportal/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=pe_mtc_018_red_vial_departamental_dic23&maxFeatures=1000&%20target=",
-    "red_vial_departamental_2024" = "https://swmapas.mtc.gob.pe:8443/geoserver/geoportal/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=pe_mtc_018_red_vial_departamental_dic24&maxFeatures=1000&%20target=",
-    "red_vial_nacional_2022" = "https://swmapas.mtc.gob.pe:8443/geoserver/geoportal/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=pe_mtc_018_red_vial_nacional_dic22&maxFeatures=1000&%20target=",
-    "red_vial_nacional_2023" = "https://swmapas.mtc.gob.pe:8443/geoserver/geoportal/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=pe_mtc_018_red_vial_nacional_dic23&maxFeatures=1000&%20target=",
-    "red_vial_vecinal_2022" = "https://swmapas.mtc.gob.pe:8443/geoserver/geoportal/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=pe_mtc_018_red_vial_vecinal_dic22&maxFeatures=1000&%20target=",
-    "red_vial_vecinal_2022" = "https://swmapas.mtc.gob.pe:8443/geoserver/geoportal/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=pe_mtc_018_red_vial_vecinal_dic23&maxFeatures=1000&%20target=",
-    "rios_selva" = "https://swmapas.mtc.gob.pe:8443/geoserver/geoportal/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=gli_ogpp_hidrovia&maxFeatures=500&%20target=",
-    "terminales_portuarios_2022" = "https://swmapas.mtc.gob.pe:8443/geoserver/geoportal/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=pe_mtc_018_terminales_portuarios_dic22&%20target=",
-    "terminales_portuarios_2023" = "https://swmapas.mtc.gob.pe:8443/geoserver/geoportal/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=pe_mtc_018_terminales_portuarios_dic23&%20target=",
-    "terminales_terrestres" = "https://swmapas.mtc.gob.pe:8443/geoserver/geoportal/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=gpt_ogpp_terminal_terrestre&maxFeatures=500&%20target=",
-    "truck_centers" = "https://swmapas.mtc.gob.pe:8443/geoserver/geoportal/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=gpt_ogpp_truck_center&maxFeatures=500&%20target=",
-    "tuneles" = "https://swmapas.mtc.gob.pe:8443/geoserver/geoportal/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=gpt_ogpp_tunel&maxFeatures=500&%20target=",
-    "unidades_peaje_2022" = "https://swmapas.mtc.gob.pe:8443/geoserver/geoportal/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=pe_mtc_018_peajes_dic22&%20target=",
-    "unidades_peaje_2023" = "https://swmapas.mtc.gob.pe:8443/geoserver/geoportal/wfs?service=WFS&version=1.0.0&request=GetFeature&typeName=pe_mtc_018_peajes_dic23&%20target="
-    )
+  mtc_layer <- getOption("mtc", default = .internal_urls$mtc)
   if (!type %in% names(mtc_layer) || is.null(type)) {
     stop("Invalid type. Please choose one layer according sernanp layer. More information use `get_data_sources(providers = 'MTC')`")
   }
-
   return(mtc_layer[[type]])
 
 }
@@ -192,17 +124,10 @@ get_mtc_link <- \(type = NULL){
 #' @keywords internal
 
 get_inaigem_link <-  \(type = NULL){
-  inaigem_layer <- c(
-    "glaciares_1989" = "https://services6.arcgis.com/c9GFTG11debu0wsH/ArcGIS/rest/services/Glaciares_1989/FeatureServer/22/query",
-    "glaciares_2018" = "https://services6.arcgis.com/c9GFTG11debu0wsH/ArcGIS/rest/services/Glaciares_2018/FeatureServer/20/query",
-    "glaciares_2020" = "https://services6.arcgis.com/c9GFTG11debu0wsH/ArcGIS/rest/services/Glaciares_2020/FeatureServer/0/query",
-    "glaciares_2023" = "https://services6.arcgis.com/c9GFTG11debu0wsH/ArcGIS/rest/services/Inventario_Nacional_de_Glaciares_y_Lagunas_de_Origen_Glaciar_Actualizado_al_a%c3%b1o_2020/FeatureServer/1/query",
-    "lagunas_con_riesgo_desborde" = "https://services6.arcgis.com/c9GFTG11debu0wsH/ArcGIS/rest/services/LGRD2024_/FeatureServer/0/query")
-
+  inaigem_layer <- getOption("inaigem", default = .internal_urls$inaigem)
   if (!type %in% names(inaigem_layer) || is.null(type)) {
     stop("Invalid type. Please choose one layer according INAIGEM layer. More information use `get_data_sources(providers = 'INAIGEM')`")
   }
-
   return(inaigem_layer[[type]])
 }
 
@@ -212,15 +137,9 @@ get_inaigem_link <-  \(type = NULL){
 #' @keywords internal
 
 get_hazard_link <-  \(type = NULL){
-  peligros_layer <- c(
-    "inundacion_inventario" = "https://sigrid.cenepred.gob.pe/arcgis/rest/services/Cartografia_Peligros/MapServer/5010100/query",
-    "inundacion_tramos_criticos" = "https://sigrid.cenepred.gob.pe/arcgis/rest/services/Cartografia_Peligros/MapServer/5010600/query",
-    "inundacion_puntos_criticos" = "https://sigrid.cenepred.gob.pe/arcgis/rest/services/Cartografia_Peligros/MapServer/5010200/query",
-    "movimiento_masa_inventario" = "https://sigrid.cenepred.gob.pe/arcgis/rest/services/Cartografia_Peligros/MapServer/5020100/query"
-    )
+  peligros_layer <- getOption("sigrid", default = .internal_urls$sigrid)
   if (!type %in% names(peligros_layer) || is.null(type)) {
     stop("Invalid type. Please choose one layer according SIGRID layer. More information use `get_data_sources(providers = 'SIGRID')`")
   }
-
   return(peligros_layer[[type]])
 }
